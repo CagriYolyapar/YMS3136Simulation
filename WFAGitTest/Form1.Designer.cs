05707e8987cfdabeda7efb6d25276d37c91eec7e
bayesiian@gmail.com <bayesiian@gmail.com>
2020-02-03 11:49:05 +0300

    Arka Palan Resmi Dgistirildi...

--- WFAGitTest/Form1.Designer.cs
+++ WFAGitTest/Form1.Designer.cs
@@ -40,10 +40,9 @@
             // 
             // btnSelamla
             // 
-            this.btnSelamla.Location = new System.Drawing.Point(371, 186);
-            this.btnSelamla.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnSelamla.Location = new System.Drawing.Point(278, 151);
             this.btnSelamla.Name = "btnSelamla";
-            this.btnSelamla.Size = new System.Drawing.Size(100, 28);
+            this.btnSelamla.Size = new System.Drawing.Size(75, 23);
             this.btnSelamla.TabIndex = 0;
             this.btnSelamla.Text = "Selamla";
             this.btnSelamla.UseVisualStyleBackColor = true;
@@ -51,10 +50,9 @@
             // 
             // btnCagri
             // 
-            this.btnCagri.Location = new System.Drawing.Point(564, 86);
-            this.btnCagri.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnCagri.Location = new System.Drawing.Point(423, 70);
             this.btnCagri.Name = "btnCagri";
-            this.btnCagri.Size = new System.Drawing.Size(100, 28);
+            this.btnCagri.Size = new System.Drawing.Size(75, 23);
             this.btnCagri.TabIndex = 1;
             this.btnCagri.Text = "btnCagri";
             this.btnCagri.UseVisualStyleBackColor = true;
@@ -62,10 +60,9 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(948, 315);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Location = new System.Drawing.Point(711, 256);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 2;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
@@ -73,29 +70,25 @@
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(33, 27);
-            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.comboBox1.Location = new System.Drawing.Point(25, 22);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(160, 24);
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
             this.comboBox1.TabIndex = 3;
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.ComboBox1_SelectedIndexChanged);
             // 
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 16;
-            this.listBox1.Location = new System.Drawing.Point(33, 261);
-            this.listBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.listBox1.Location = new System.Drawing.Point(25, 212);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(159, 116);
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
             this.listBox1.TabIndex = 4;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(383, 348);
-            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Location = new System.Drawing.Point(287, 283);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(100, 28);
+            this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 5;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
@@ -103,10 +96,9 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(624, 225);
-            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button3.Location = new System.Drawing.Point(468, 183);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(100, 28);
+            this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 6;
             this.button3.Text = "button3";
             this.button3.UseVisualStyleBackColor = true;
@@ -114,17 +106,18 @@
             // lstanil
             // 
             this.lstanil.FormattingEnabled = true;
-            this.lstanil.ItemHeight = 16;
-            this.lstanil.Location = new System.Drawing.Point(543, 365);
+            this.lstanil.Location = new System.Drawing.Point(407, 297);
+            this.lstanil.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.lstanil.Name = "lstanil";
-            this.lstanil.Size = new System.Drawing.Size(120, 84);
+            this.lstanil.Size = new System.Drawing.Size(91, 69);
             this.lstanil.TabIndex = 7;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.BackgroundImage = global::WFAGitTest.Properties.Resources.Annotation_2020_02_03_104617;
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.lstanil);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -133,7 +126,6 @@
             this.Controls.Add(this.button1);
             this.Controls.Add(this.btnCagri);
             this.Controls.Add(this.btnSelamla);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
