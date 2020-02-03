a5455f1b5985376ddb5e5919169678da587038e4
barisbdci@gmail.com <barisbdci@gmail.com>
2020-02-03 13:01:21 +0300

    Class oluşturuldu

---
+++ WFAGitTest/Models/Barisbdi.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WFAGitTest.Models
+{
+    public class Barisbdi
+    {
+        public string Ad { get; set; }
+        public string Soyad{ get; set; }
+        public int Maas { get; set; }
+    }
+}
