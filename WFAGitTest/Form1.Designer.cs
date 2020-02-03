3162ecff427f423bb4fec15263df1f2aded9c104
Ramadan Özezen <ramadan_lnt@hotmail.com>
2020-02-03 10:56:13 +0300

    Buton eklendi, event kodları yazıldı.

--- WFAGitTest/Form1.Designer.cs
+++ WFAGitTest/Form1.Designer.cs
@@ -28,13 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnSelamla = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSelamla
+            // 
+            this.btnSelamla.Location = new System.Drawing.Point(278, 151);
+            this.btnSelamla.Name = "btnSelamla";
+            this.btnSelamla.Size = new System.Drawing.Size(75, 23);
+            this.btnSelamla.TabIndex = 0;
+            this.btnSelamla.Text = "Selamla";
+            this.btnSelamla.UseVisualStyleBackColor = true;
+            this.btnSelamla.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSelamla);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnSelamla;
     }
 }
 
